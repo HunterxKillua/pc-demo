8ac97d2c8ee67425328ee208a6fc859bdb6a3bea
killua <chengjie@wangchengjiedebijibendiannao.local>
2025-09-19 18:03:25 +0800

    feat: 增加面包屑导航

--- src/components/layout/components/header.vue
+++ src/components/layout/components/header.vue
@@ -1,23 +1,59 @@
-<script setup lang="tsx">
+<script setup lang="ts">
+import { ElBreadcrumb, ElBreadcrumbItem } from 'element-plus'
+
 const appStore = useAppStore()
+
 const { toggleMenu } = appStore
 const { isCollapse } = storeToRefs(appStore)
-defineRender(() => {
-  return (
-    <div class="layout-header">
-      <el-button onClick={toggleMenu}>
-        { isCollapse.value ? '展开' : '收起' }
-      </el-button>
-      header
-    </div>
-  )
+const route = useRoute()
+
+const breadcrumbs = computed<{
+  name: string
+  path: string
+  title: string
+  disabled: boolean
+}[]>(() => {
+  return route.matched
+    .filter(r => r.meta?.title && r.name && r.path !== '/')
+    .map((r, index, arr) => {
+      const currentPath = route.path
+      const parentRedirect = arr[index - 1]?.redirect
+      const disabled = r.path === currentPath || parentRedirect === currentPath
+
+      return {
+        name: r.name as string,
+        path: r.path,
+        title: r.meta.title as string,
+        disabled,
+      }
+    })
 })
 </script>
+
+<template>
+  <div class="layout-header">
+    <ElButton class="mr-8" @lick="toggleMenu">
+      {{ isCollapse ? '展开' : '收起' }}
+    </ElButton>
+    <ElBreadcrumb separator="/">
+      <ElBreadcrumbItem
+        v-for="crumb in breadcrumbs"
+        :key="crumb.name"
+      >
+        <RouterLink v-if="!crumb.disabled" :to="crumb.path">
+          {{ crumb.title }}
+        </RouterLink>
+        <span v-else>{{ crumb.title }}</span>
+      </ElBreadcrumbItem>
+    </ElBreadcrumb>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .layout-header {
   width: 100%;
   height: 50px;
   background-color: #fff;
+  @apply flex items-center;
 }
 </style>
